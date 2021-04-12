bd236e12f7aa968adb27ec69f4971e292dbf73c4
dmircead <dmircead@yahoo.com>
2021-04-12 22:47:23 +0300

    adding files for githup pages

---
+++ src/aiurea.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class aiurea extends Component {
+    rener(){
+        return (
+            <div>
+                <h1>Learning react ...</h1>
+            </div>
+        )
+    }
+}
+                
